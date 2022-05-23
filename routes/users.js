4f3a43d9a355bed7bd3e41ef2e9c9b688ba8ed14
Elaine Ly <elainely2003@gmail.com>
2022-05-23 08:58:05 -0400

    Inserted parameter validation

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // get request
 // post request
@@ -10,8 +13,25 @@ const router = express.Router();
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    // validation from express-validator
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with six or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('passed');
+    // limit what can be sent and ensure that certain things get through with express validator
+  }
+);
 
 module.exports = router;
