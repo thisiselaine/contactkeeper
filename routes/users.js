483afdc48221d7de60ccc4716ad89daf2c946b70
Elaine Ly <elainely2003@gmail.com>
2022-05-23 21:19:52 -0400

    implemented password hashing and user saving to mongodb

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 // Functionality: contains the register
 
+// needed constants and libraries
+const bcrypt = require('bcryptjs/dist/bcrypt');
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
@@ -24,13 +26,42 @@ router.post(
       'please enter a password with six or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('passed');
+    // res.send('passed');
     // limit what can be sent and ensure that certain things get through with express validator
+
+    const { name, email, password } = req.body;
+    try {
+      // mongoose feature: find email
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // create container for user
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // need salt to encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // put user into database
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
